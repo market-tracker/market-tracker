222a2192a9d8326eadfcd8af2051d4336210b69f
bperezgo <bsperezgo@gmail.com>
2021-10-24 20:14:23 -0500

    feat: first connection to a websocket, todo: refactor websocket pkg

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 
 	"github.com/market-tracker/market-tracker/config"
 	"github.com/market-tracker/market-tracker/server"
+	"github.com/market-tracker/market-tracker/wsTiingo"
 )
 
 func init() {
-	fmt.Println("First of all")
+	// Containers of global dependencies
+	log.SetFlags(0)
+	ctx := context.Background()
+	c := config.GetConfiguration()
+	tiingoOpts := &wsTiingo.TiingoOptions{
+		EventName:     "subscribe",
+		Authorization: c.TiingoApiToken,
+		EventData: &wsTiingo.EventDataTiingo{
+			ThresholdLevel: 5,
+		},
+	}
+	ws := wsTiingo.GetWsTiingo(ctx, c.TiingoApiUrl, tiingoOpts)
+
+	// run in a go rutine because in the subscription, the subscriber is waiting
+	// for msgs
+	go ws.Subscribe(ctx)
 }
 
 func main() {
